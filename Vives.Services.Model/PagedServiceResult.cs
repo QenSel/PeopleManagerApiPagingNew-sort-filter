1ee2c5a7f3b5d28e514e480f3f9c0ed653316317
QenSel <125572813+QenSel@users.noreply.github.com>
2023-10-18 13:42:45 +0200

    Initial commit

---
+++ Vives.Services.Model/PagedServiceResult.cs
@@ -0,0 +1,11 @@
+﻿using Vives.Services.Model.Abstractions;
+
+namespace Vives.Services.Model
+{
+    public class PagedServiceResult<T>: ServiceResult, IPagedServiceResult
+    {
+        public IList<T> Data { get; set; } = new List<T>();
+        public required Paging Paging { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
